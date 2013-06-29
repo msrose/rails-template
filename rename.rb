eea6350c615bf23a3489c9653892c21ee6c7f287
Michael Rose <ekimsc1094@sympatico.ca>
2013-06-28 22:30:30 -0400

    Edit Rakefile and README in rename file.

--- rename.rb
+++ rename.rb
@@ -10,8 +10,17 @@ new_name_camelized = new_name.split('_').map(&:capitalize).join
  './config/environments/production.rb',
  './config/initializers/secret_token.rb',
  './config/initializers/session_store.rb',
- './config/routes.rb'
+ './config/routes.rb',
+ './Rakefile'
 ].each do |file|
+  puts "Renaming #{file}..."
   text = File.read(file).gsub('RailsTemplate', new_name_camelized).gsub('rails_template', new_name)
   File.open(file, 'w') { |f| f.puts text }
 end
+
+puts "Creating a new README"
+File.open("README.md", "w") do |f|
+  f.puts "##{new_name_camelized}\nCreated with the rails template from https://github.com/msrose/rails_template"
+end
+
+puts "Project successfully renamed to #{new_name_camelized}"
