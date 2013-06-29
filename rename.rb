f9d3dc9f8509c31cd9ead05b68573dace8b74f6d
Michael Rose <ekimsc1094@sympatico.ca>
2013-06-28 22:16:06 -0400

    Fix bugs in rename file and do some refactoring.

--- rename.rb
+++ rename.rb
@@ -1,27 +1,17 @@
-files = [ 'app/view/layouts/application.html.haml',
-          'config.ru',
-          'config/application.rb',
-          'config/environment.rb',
-          'config/environments/development.rb',
-          'config/environments/test.rb',
-          'config/environments/production.rb',
-          'config/initializer/secret_token.rb',
-          'config/initializer/session_store.rb',
-          'config/routes.rb'
-        ]
-
 new_name = ARGV[0]
 new_name_camelized = new_name.split('_').map(&:capitalize).join
 
-files.each do |file|
-  text = ''
-  File.open(file, 'r') do |f|
-    while line = f.gets
-      text << line
-    end
-  end
-
-  f = File.open(file, 'w')
-  f.write(text.gsub('RailsTemplate', new_name_camelized).gsub('rails_template', new_name))
-  file.close
+['./app/views/layouts/application.html.haml',
+ './config.ru',
+ './config/application.rb',
+ './config/environment.rb',
+ './config/environments/development.rb',
+ './config/environments/test.rb',
+ './config/environments/production.rb',
+ './config/initializers/secret_token.rb',
+ './config/initializers/session_store.rb',
+ './config/routes.rb'
+].each do |file|
+  text = File.read(file).gsub('RailsTemplate', new_name_camelized).gsub('rails_template', new_name)
+  File.open(file, 'w') { |f| f.puts text }
 end
